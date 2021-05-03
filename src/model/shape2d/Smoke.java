dbd72b8bc9546511a67344cc7d20e7716748ca3f
Minh Tu <minhtu1392000@gmail.com>
2021-05-03 20:13:09 +0700

    hoan thanh ve khoi

--- src/model/shape2d/Smoke.java
+++ src/model/shape2d/Smoke.java
@@ -6,6 +6,7 @@
 package model.shape2d;
 
 import java.awt.Color;
+import java.util.ArrayList;
 
 /**
  *
@@ -21,7 +22,7 @@ public class Smoke extends Shape2D{
         this.filledColor = new Color (255,0,0);
         savePoint(this.startPoint.coordX, this.startPoint.coordY);
         this.filledColor = new Color (0,0,0);
-        
+        //khói ở trong
         this.drawOutlineCircle(2, new Point2D (this.startPoint,-6,2), false, false, false, false, false, true, true, true);  
         this.drawOutlineCircle(2, new Point2D (this.startPoint,-6 -2,2 + 3), false, false, false, false, false, false, true, true); 
         
@@ -29,6 +30,50 @@ public class Smoke extends Shape2D{
         //vòng tròn liên tiếp
         this.drawOutlineCircle(4, new Point2D (this.startPoint,4,-4), true, true, false, false, false, false, false, true);   
         this.drawOutlineCircle(2, new Point2D (this.startPoint,4 +6,-4), true, true, true, false, false, false, false, true);
+        
+        this.drawOutlineCircle(4, new Point2D (this.startPoint,0,6), false, false, false, false, true, true, false, false);  
+        
+        //viền ngoài, theo chiều kim đồng hồ lồng nhau
+        ArrayList<Point2D> pointList_Border = new ArrayList<>();
+        pointList_Border.add(new Point2D (this.startPoint,-10,-1));
+        this.drawOutlineCircle(7, pointList_Border.get(0), false, false, false, false, false, false, true, true); 
+        
+        pointList_Border.add(new Point2D (pointList_Border.get(0),4, -3));
+        this.drawOutlineCircle(6, pointList_Border.get(1), false, false, false, false, false, false, true, true);  
+        
+        pointList_Border.add(new Point2D(pointList_Border.get(1),3,-2));
+        this.drawOutlineCircle(7, pointList_Border.get(2), true, false, false, false, false, false, false, true); 
+        
+        pointList_Border.add(new Point2D(pointList_Border.get(2),6,-4));
+        this.drawOutlineCircle(5, pointList_Border.get(3), true, false, false, false, false, false, false, true); 
+        
+        pointList_Border.add(new Point2D(pointList_Border.get(3),5, 1));
+        this.drawOutlineCircle(7, pointList_Border.get(4), true, false, false, false, false, false, false, true); 
+        
+        pointList_Border.add(new Point2D(pointList_Border.get(4),5, 0));
+        this.drawOutlineCircle(7, pointList_Border.get(5), true, true, true, false, false, false, false, false); 
+        
+        pointList_Border.add(new Point2D(pointList_Border.get(5),3, 5));
+        this.drawOutlineCircle(5, pointList_Border.get(6), false, true, true, true, false, false, false, false); 
+        
+        pointList_Border.add(new Point2D(pointList_Border.get(6),1, 8));
+        this.drawOutlineCircle(5, pointList_Border.get(7), true, true, true, false, false, false, false, false); 
+        
+        //quay về đầu, làm bên trái
+        pointList_Border.add(new Point2D(pointList_Border.get(0),-3, 6));
+        this.drawOutlineCircle(7, pointList_Border.get(8), false, false, false, false, true, true, true, true); 
+        
+        pointList_Border.add(new Point2D(pointList_Border.get(8),4, 4));
+        this.drawOutlineCircle(7, pointList_Border.get(9), false, false, false, false, true, true, false, false); 
+        
+        //quay lại bên phải làm cái cuối cùng trước khi vẽ ellipse
+        pointList_Border.add(new Point2D(pointList_Border.get(7),-3, 4));
+        this.drawOutlineCircle(6, pointList_Border.get(10), false, true, true, true, false, false, false, false); 
+        
+        this.drawOutlineEllipse(6, 10, new Point2D (pointList_Border.get(10),2,12), true, false, false, false);
+        this.drawOutlineEllipse(6, 10, new Point2D (pointList_Border.get(10),-25,12), false, true, false, false);
+        
+        
     }
     @Override
     public void applyMove(Vector2D vector) {
