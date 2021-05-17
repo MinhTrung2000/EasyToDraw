681e1783cdcd7158a35060e078210ab8d858d277
Minh Trung <mt10tmt@gmail.com>
2021-05-17 21:10:57 +0700

    Fix line style error

--- src/control/SettingConstants.java
+++ src/control/SettingConstants.java
@@ -17,6 +17,11 @@ public interface SettingConstants {
                 return "────────────";
             }
         },
+        DOT {
+            public String toString() {
+                return "∙ ∙ ∙ ∙ ∙ ∙ ∙ ∙ ∙ ∙ ∙ ∙ ∙ ∙";
+            }
+        },
         DASH {
             public String toString() {
                 return "─── ─── ─── ──";
@@ -37,7 +42,6 @@ public interface SettingConstants {
                 return "───────────→";
             }
         },
-        DOT,
     }
 
     public enum DrawingToolMode {
@@ -208,7 +212,7 @@ public interface SettingConstants {
 
     public static final int COORD_X_O = (int) (WIDTH_DRAW_AREA / 2) - 1;
     public static final int COORD_Y_O = (int) (HEIGHT_DRAW_AREA / 2) - 1;
-    
+
     public static final int DEFAULT_LINE_SIZE = 1;
 
     public static final boolean DEFAULT_VISUAL_SHOW_GRID = true;
